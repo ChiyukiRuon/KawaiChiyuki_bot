189e2a67f7afc08b848e33b354333c7905f8a871
ChiyukiRuon <1275846107@qq.com>
2022-10-21 22:27:39 +0800

    2022.10.21 - 实现了随机获取Pixiv图片 - 完善了控制台输出的日志信息，在发送图片失败时会发送错误信息

--- GetPixivImage.py
+++ GetPixivImage.py
@@ -1,6 +1,12 @@
+import datetime
 import json
+import random
 
+import pytz
 import requests
+
+tz = pytz.timezone('Asia/Shanghai')
+now_time = datetime.datetime.now(tz).strftime('%Y-%m-%d %H:%M:%S')
 
 
 def pixiv_rank(rank_type):
@@ -29,34 +35,66 @@ def pixiv_rank(rank_type):
 
     resp_list = []
     url = 'https://api.obfs.dev/api/pixiv/rank?mode={}'.format(rank_type)
-    r = json.loads(requests.get(url).content)
-    res_list = r.get('illusts')
-    for i in range(len(res_list)):
-        pid = res_list[i].get('id')
-        title = res_list[i].get('title')
-        username = res_list[i].get('user').get('name')
-        if len(res_list[i].get('meta_pages')) != 0:
-            url = res_list[i].get('meta_pages')[0].get('image_urls').get('medium')  # 多张图片只获取第一张
-            num = len(res_list[i].get('meta_pages'))
-        else:
-            url = res_list[i].get('meta_single_page').get('original_image_url')
-            num = 1
+    pic_json = json.loads(requests.get(url).content)
+    pic_list = pic_json.get('illusts')
+    if pic_list is None:
+        resp_list = [pic_info(pic_json, 1)]
+    else:
+        for i in range(len(pic_list)):
+            response = pic_info(pic_json, i)
 
-        response = {
-            'pid': pid,  # 图片ID
-            'title': title,  # 图片标题
-            'username': username,  # 作者
-            'url': url,  # 图片下载链接
-            'num': num,  # 图片数量
-        }
-
-        resp_list.append(response)
+            resp_list.append(response)
 
     return resp_list
 
 
-# TODO 从Pixiv获取一张随机图片
 def random_pic():
-    url = json.loads(requests.get('https://api.btstu.cn/sjbz/api.php?lx=dongman&format=json').content).get('imgurl')
+    tags_url = 'https://api.obfs.dev/api/pixiv/tags'
+    search_url = 'https://api.obfs.dev/api/pixiv/search?word='
+    user = ['1000', '3000', '5000', '7500', '10000', '30000', '50000']
+    tag_list = json.loads(requests.get(tags_url).content).get('trend_tags')
+    while True:
+        try:
+            rand_user = user[random.randint(0, 6)]
+            rand_tag = random.randint(0, 39)
+            tag = tag_list[rand_tag].get('tag')
+            pic_json = json.loads(requests.get(search_url + tag + ' ' + rand_user + 'users入り').content)
+            print('{}:"random_pic"tag:{},user:{}'.format(now_time, tag, rand_user))
+            rand_pic = random.randint(0, 29)
+            response = pic_info(pic_json, rand_pic)
+            print('{}:"random_pic"resp:{}'.format(now_time, response))
+            if len(response) != 0: break
+        except Exception as e:
+            print('{}:"random_pic"{}'.format(now_time, e))
 
-    return url
+    return response
+
+
+def pic_info(pic_json, key):
+    pic_list = pic_json.get('illusts')
+    if pic_list is None:
+        msg = pic_json.get('error').get('message')
+        print('{}:"pic_info"获取图片时发生错误{}'.format(now_time, msg))
+        response = {
+            'message': msg
+        }
+    else:
+        title = pic_list[key].get('title')
+        pid = pic_list[key].get('id')
+        username = pic_list[key].get('user').get('name')
+        if len(pic_list[key].get('meta_pages')) != 0:
+            url = pic_list[key].get('meta_pages')[0].get('image_urls').get('medium')
+            num = len(pic_list[key].get('meta_pages'))
+        else:
+            url = pic_list[key].get('meta_single_page').get('original_image_url')
+            num = 1
+
+        response = {
+            'pid': pid,
+            'title': title,
+            'username': username,
+            'url': url,
+            'num': num
+        }
+
+    return response
